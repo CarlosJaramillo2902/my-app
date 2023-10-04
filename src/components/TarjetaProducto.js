f5c3bb9e1ce1735976efd6ed6dcce4e9f4da8dbf
CarlosJaramillo2902 <carlosjagu@unisabana.edu.co>
2023-10-04 18:44:47 -0500

    Implementar UseContext

--- src/components/TarjetaProducto.js
+++ src/components/TarjetaProducto.js
@@ -1,38 +1,32 @@
-import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
+import { useProducts } from "../context";
 import './TarjetaProducto.css';
 
 function TarjetaProducto() {
-  const [product, setProduct] = useState([]);
   const { id } = useParams();
-  console.log("id-producto", id);
-
-  useEffect(() => {
-    fetch(`https://fakestoreapi.com/products/${id}`)
-      .then((response) => response.json())
-      .then((data) => setProduct(data));
-  }, [id]);
+  const { setProductId, selectedProduct } = useProducts();
+  setProductId(id);
 
   const añadirCarrito = () => {
     const carritoActual = JSON.parse(localStorage.getItem("carrito")) || [];
-    carritoActual.push(product);
+    carritoActual.push(selectedProduct);
     localStorage.setItem("carrito", JSON.stringify(carritoActual));
   }
 
   return (
     <>
-      {product ? (
+      {selectedProduct ? (
         <>
-        <div className="contenedorT">
-          <h2>
-            <i>{product.title} </i>
-            por {product.price}
-          </h2>
-          <img width="150" height="350" he src={product.image} alt={product.category} />
-          <p>{product.description}</p>
-          <Link to={"/"} className="boton">Volver</Link>
-          <Link to={"/"} onClick={añadirCarrito} className="botonCarrito">Añadir al Carrito</Link>
-        </div>
+          <div className="contenedorT">
+            <h2>
+              <i>{selectedProduct.title} </i>
+              por {selectedProduct.price}
+            </h2>
+            <img width="150" height="350" he src={selectedProduct.image} alt={selectedProduct.category} />
+            <p>{selectedProduct.description}</p>
+            <Link to={"/"} className="boton">Volver</Link>
+            <Link to={"/"} onClick={añadirCarrito} className="botonCarrito">Añadir al Carrito</Link>
+          </div>
         </>
       ) : (
         <div> loading... </div>
